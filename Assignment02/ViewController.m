37829dfbc46bfd73a3a7de90b4fe114d5fce3c73
JIHOON PARK <ampo2787@cnu.ac.kr>
2018-11-12 13:16:04 +0900

    Initial Commit

---
+++ Assignment02/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Assignment02
+//
+//  Created by JihoonPark on 12/11/2018.
+//  Copyright © 2018 JihoonPark. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
